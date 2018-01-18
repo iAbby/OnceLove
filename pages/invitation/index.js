f8745f54694925b18cef1ae8b05ef789afeeee89
donghaikun <allen6699@vip.qq.com>
2018-01-18 18:04:20 +0800

    更新部分细节

--- pages/invitation/index.js
+++ pages/invitation/index.js
@@ -112,35 +112,14 @@ Page({
       }
     }
   },
-  // 触摸开始事件  
-  touchStart: function (e) {
-    touchDot = e.touches[0].pageX; // 获取触摸时的原点  
-    // 使用js计时器记录时间    
-    interval = setInterval(function () {
-      time++;
-    }, 100);
+  callhe: function (event) {
+    wx.makePhoneCall({
+      phoneNumber: this.data.mainInfo.he_tel
+    })
   },
-  // 触摸移动事件  
-  touchMove: function (e) {
-    var touchMove = e.touches[0].pageX;
-    console.log("touchMove:" + touchMove + " touchDot:" + touchDot + " diff:" + (touchMove - touchDot));
-    // 向左滑动    
-    if (touchMove - touchDot <= -40 && time < 10) {
-      wx.switchTab({
-        url: '/pages/map/index'
-      });
-    }
-    // 向右滑动  
-    if (touchMove - touchDot >= 40 && time < 10) {
-      console.log('向右滑动');
-      wx.switchTab({
-        url: '/pages/index/index'
-      });
-    }
-  },
-  // 触摸结束事件  
-  touchEnd: function (e) {
-    clearInterval(interval); // 清除setInterval  
-    time = 0;
-  },  
+  callshe: function (event) {
+    wx.makePhoneCall({
+      phoneNumber: this.data.mainInfo.she_tel
+    })
+  }
 })
